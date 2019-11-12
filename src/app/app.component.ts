2728f076834a55fb7a82de6769dac04e6281ec26
David Rivas <david.rivas@pevaar.com>
2019-11-12 17:40:29 -0500

    App component setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Character } from './model/character';
+import { CharacterService } from './services/character.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    characters: Character[];
+
+    constructor(private characterService: CharacterService) {}
+
+    ngOnInit(): void {
+        this.characterService.getCharacters().subscribe(
+            result => this.characters = result.data.results,
+            error => alert('An error occured')
+        );
+    }
 }
