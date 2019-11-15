1f5eab050905973ddacbef49fa0b00e10d31cde5
David Rivas <david.rivas@pevaar.com>
2019-11-15 08:24:57 -0500

    Search bar added

--- src/app/services/challenge.service.ts
+++ src/app/services/challenge.service.ts
@@ -1,5 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
 export class ChallengeService {
-    public static PublicKey: string = 'f1def8f826359cbe621637efac4cf74c';
-    public static MD5Hash: string = 'fda0c911fa2baa3c9a36f36433acb8b4';
-    public static MaxResultsPerPage: number = 10;
+    public publicKey: string = 'f1def8f826359cbe621637efac4cf74c';
+    public md5Hash: string = 'fda0c911fa2baa3c9a36f36433acb8b4';
+    public maxResultsPerPage: number = 10;
 }
--- src/app/services/character.service.ts
+++ src/app/services/character.service.ts
@@ -10,10 +10,10 @@ import { ChallengeService } from './challenge.service';
 export class CharacterService {
     private readonly baseUrl: string = 'https://gateway.marvel.com/v1/public/characters';
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private challengeService: ChallengeService) { }
 
     getCharacters(): Observable<CharacterResponse> {
-        const url: string = `${this.baseUrl}?ts=1&apikey=${ChallengeService.PublicKey}&hash=${ChallengeService.MD5Hash}`;
+        const url: string = `${this.baseUrl}?ts=1&apikey=${this.challengeService.publicKey}&hash=${this.challengeService.md5Hash}`;
 
         return this.http.get<CharacterResponse>(url);
     }
